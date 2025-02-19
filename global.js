5274f45b43bc1a18c8f00fc0d467c226b2a08eb4
Sreekar Ramisetty <hindi6476@gmail.com>
2025-02-19 12:54:40 -0600

    blobal.js

--- global.js
+++ global.js
@@ -1,9 +1,15 @@
-document.getElementsByTagName("body").innerHTML += `<!-- Google tag (gtag.js) -->
-<script async src="https://www.googletagmanager.com/gtag/js?id=G-L6VPS22YJG"></script>
-<script>
-  window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);}
-  gtag('js', new Date());
+window.dataLayer = window.dataLayer || [];
+function gtag() { dataLayer.push(arguments); }
+gtag('js', new Date());
+gtag('config', 'G-L6VPS22YJG');
 
-  gtag('config', 'G-L6VPS22YJG');
-</script>`;
+
+fetch(`https://home.calschwick.net/analytics/submit?type=sreekar-website&url=${encodeURIComponent(window.location)}`, {
+
+  method: "POST",
+
+}).then(res => {
+
+  console.log("Request complete! response:", res);
+
+});
