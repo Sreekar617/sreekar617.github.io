733d10d86ce589f4e48006d3032fe3c0d8286756
Sreekar Ramisetty <hindi6476@gmail.com>
2025-02-19 12:52:18 -0600

    merg

--- add_global_script.py
+++ add_global_script.py
@@ -4,31 +4,33 @@ path = "."
 indicator_string_start = "<!-- global script -->"
 indicator_string_end = "<!-- end global script -->"
 stuff2add = """
+<!-- Google tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-L6VPS22YJG"></script>
 <script src="/global.js"></script>
 """
 files: list[str] = [os.path.join(dp, f) for dp, dn, fn in os.walk(path) for f in fn]
 
-files_changed=0
-files_failed=0
+files_changed = 0
+files_failed = 0
 
 for fn in files:
     try:
         if fn.endswith(".html"):
             print(fn)
-            with open(fn, 'r') as f:
-                text=f.read()
-            indicator_idx=text.find(indicator_string_start)
+            with open(fn, "r") as f:
+                text = f.read()
+            indicator_idx = text.find(indicator_string_start)
             if indicator_idx != -1:
                 print("  im skippin")
             text += indicator_string_start + "\n"
             text += stuff2add
             text += indicator_string_end + "\n"
-            with open(fn, 'w') as f:
+            with open(fn, "w") as f:
                 f.write(text)
-            files_changed+=1
+            files_changed += 1
     except:
         print(f"womp womp: {fn}")
-        files_failed+=1
+        files_failed += 1
 
 print("im done")
 print(f"{files_changed=}")
---
+++ global.js
@@ -0,0 +1,9 @@
+document.getElementsByTagName("body").innerHTML += `<!-- Google tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-L6VPS22YJG"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-L6VPS22YJG');
+</script>`;
